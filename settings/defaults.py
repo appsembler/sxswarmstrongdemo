8cff1814b0b11e625c052f157cd87647a3c9c9cd
Nate Aune <natea@jazkarta.com>
2012-05-25 22:57:58 -0300

    Update settings/defaults.py

--- settings/defaults.py
+++ settings/defaults.py
@@ -34,6 +34,8 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
+ROOT_URLCONF = 'urls'
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = project_dir("media")
